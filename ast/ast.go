74fc13c71ebf8f62e4ff23d9c9fdb68d1a348ec6
joshuwi2 <joshuwi2@cisco.com>
2018-09-14 15:00:34 -0400

    Finished 2.6.4

--- ast/ast.go
+++ ast/ast.go
@@ -1,15 +1,17 @@
 package ast
 
 import "monkey_interpreter/token"
+import "bytes"
 
 // Node interface to represent node
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement interface to represent statements
 type Statement interface {
-	TokenLiteral() string
+	Node
 	statementNode()
 }
 
@@ -32,6 +34,16 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement structure
 type LetStatement struct {
 	Token token.Token // the token.LET token
@@ -44,6 +56,22 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral and statementNode implement Statement interface for LetStatement
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 // Identifier struct used to track identifiers
@@ -59,6 +87,8 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string { return i.Value }
+
 // ReturnStatement structure
 type ReturnStatement struct {
 	Token       token.Token // the 'return' token
@@ -69,3 +99,36 @@ func (rs *ReturnStatement) statementNode() {}
 
 // TokenLiteral and statementNode implement statement interface for ReturnStatement
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement struct
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral and statementNode implement statement interface for ExpressionStatement
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
